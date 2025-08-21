548c58902b96ad0ad4fdbd385c3b671457ced1db
tmmcmasters <tmmcmasters@gmail.com>
2025-08-21 13:30:12 -0400

    Made the drawer at-least appear :)

--- client/components/drawer/NavigationDrawer.vue
+++ client/components/drawer/NavigationDrawer.vue
@@ -3,6 +3,12 @@ import { IconMenuDeep } from '@tabler/icons-vue'
 import Button from '../ui/button/Button.vue'
 import Drawer from '../ui/drawer/Drawer.vue'
 import DrawerTrigger from '../ui/drawer/DrawerTrigger.vue'
+import DrawerContent from '../ui/drawer/DrawerContent.vue'
+import DrawerHeader from '../ui/drawer/DrawerHeader.vue'
+import DrawerTitle from '../ui/drawer/DrawerTitle.vue'
+import DrawerDescription from '../ui/drawer/DrawerDescription.vue'
+import DrawerFooter from '../ui/drawer/DrawerFooter.vue'
+import DrawerClose from '../ui/drawer/DrawerClose.vue'
 </script>
 
 <template>
@@ -13,6 +19,23 @@ import DrawerTrigger from '../ui/drawer/DrawerTrigger.vue'
         <IconMenuDeep class="size-5 text-foreground" />
       </Button>
     </DrawerTrigger>
+    <DrawerContent>
+      <div class="mx-auto w-full max-w-sm">
+        <DrawerHeader>
+          <DrawerTitle>Move Goal</DrawerTitle>
+          <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+        </DrawerHeader>
+        <div class="p-4 pb-0">
+          <div class="flex items-center justify-center space-x-2"></div>
+        </div>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose as-child>
+            <Button variant="outline"> Cancel </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </div>
+    </DrawerContent>
   </Drawer>
 </template>
 
